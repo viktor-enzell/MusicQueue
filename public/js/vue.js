8b5e72a3fc833061e795b061b338781b7e6ad8ab
viktor enzell <viktor@localhost.localdomain>
2018-06-29 21:26:07 +0200

    Many changes including ability to create playlist and play specific songs.

--- public/js/vue.js
+++ public/js/vue.js
@@ -5,6 +5,8 @@ var vm = new Vue({
     el: '.container',
     data: {
         params: {},
+        user_id: '',
+        queue_id: '',
         access_token: '',
         refresh_token: '',
         error: '',
@@ -12,6 +14,7 @@ var vm = new Vue({
         showQueue: true,
         showUserInfo: false,
         playing: false,
+        songHasStarted: false,
 
         responses: {},
         name: '',
@@ -34,6 +37,7 @@ var vm = new Vue({
         else this.login = true;
 
         this.authenticate();
+        this.newPlaylist();
     },
     methods: {
         getParams: function () {
@@ -59,6 +63,7 @@ var vm = new Vue({
                         success: function (response) {
                             Vue.set(vm.responses, 'user', response);
                             vm.displayUser();
+                            // vm.newQueue(); ONLY IF PLAYLIST METHOD IS USED
                         }
                     });
                 } else {
@@ -77,45 +82,104 @@ var vm = new Vue({
                 vm.displayNewToken();
             });
         },
-        play: function () {
+        newPlaylist: function () {
             $.ajax({
-                url: 'https://api.spotify.com/v1/me/player/play',
+                url: 'https://api.spotify.com/v1/users/' + this.user_id + '/playlists',
+                method: 'POST',
                 headers: {
-                    'Authorization': 'Bearer ' + this.access_token
+                    'Authorization': 'Bearer ' + this.access_token,
+                    'Content-Type': 'application/json'
                 },
+                data: "{\"name\":\"Music Queue\",\"description\":\"Your collaborative queue\"}",
                 success: function (response) {
-                    console.log(response);
+                    Vue.set(vm.responses, 'playlist', response);
+                    vm.setQueueId();
                 }
             });
+        },
+        addToPlaylist: function (song) {
+            $.ajax({
+                url: 'https://api.spotify.com/v1/users/' + this.user_id + '/playlists/' + this.queue_id + '/tracks',
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + this.access_token,
+                    'Content-Type': 'application/json'
+                },
+                data: "{\"uris\": [\"spotify:track:4iV5W9uYEdYUVa79Axb7Rh\",\"spotify:track:1301WleyT98MSxVHPZCA6M\"]}",
+                success: function (response) {
+                }
+            });
+        },
+        play: function () {
+            this.getPlayback();
+            if (this.songHasStarted) {
+                $.ajax({
+                    url: 'https://api.spotify.com/v1/me/player/play',
+                    method: 'PUT',
+                    headers: {
+                        'Authorization': 'Bearer ' + this.access_token
+                    },
+                    success: function (response) {
+                    }
+                });
+            } else {
+                this.playNext();
+            }
             this.playing = true;
         },
         pause: function () {
             $.ajax({
                 url: 'https://api.spotify.com/v1/me/player/pause',
+                method: 'PUT',
                 headers: {
                     'Authorization': 'Bearer ' + this.access_token
                 },
                 success: function (response) {
-                    console.log(response);
                 }
             });
             this.playing = false;
         },
+        playNext: function () {
+            var length = this.queue.length;
+            var i;
+            var queueString = "\"spotify:track:" + this.currentSong.id + "\"," + "\"spotify:track:" + this.queue[0].id + "\"";
+            $.ajax({
+                url: 'https://api.spotify.com/v1/me/player/play',
+                method: 'PUT',
+                headers: {
+                    'Authorization': 'Bearer ' + this.access_token,
+                    'Content-Type': 'application/json'
+                },
+                data: "{\"uris\": [" + queueString + "], \"offset\": {\"position\": 0}}",
+            });
+        },
+        getPlayback: function () {
+            $.ajax({
+                url: 'https://api.spotify.com/v1/me/player',
+                headers: {
+                    'Authorization': 'Bearer ' + this.access_token,
+                },
+                success: function (response) {
+                    Vue.set(vm.responses, 'player', response);
+                    vm.setSongHasStarted();
+                }
+            });
+        },
         search: function (query) {
-                $.ajax({
-                    url: 'https://api.spotify.com/v1/search',
-                    headers: {
-                        'Authorization': 'Bearer ' + this.access_token
-                    },
-                    data: {
-                        q: query,
-                        type: 'track'
-                    },
-                    success: function (response) {
-                        Vue.set(vm.responses, 'search', response);
-                        vm.displaySearch();
-                    }
-                });
+            $.ajax({
+                url: 'https://api.spotify.com/v1/search',
+                headers: {
+                    'Authorization': 'Bearer ' + this.access_token
+                },
+                data: {
+                    q: query,
+                    type: 'track'
+                },
+                success: function (response) {
+                    Vue.set(vm.responses, 'search', response);
+                    vm.displaySearch();
+                }
+            });
         },
         queueSong: function (song) {
             if (this.queue.length === 0 && this.currentSong === '') {
@@ -123,12 +187,14 @@ var vm = new Vue({
             } else {
                 this.queue.push(song);
             }
+            // this.addToQueue(song); ONLY IF PLAYLIST METHOD IS USED
             this.searchInput = "";
             this.showQueue = true;
         },
         displayUser: function () {
             this.name = this.responses.user.display_name;
             this.email = this.responses.user.email;
+            this.user_id = this.responses.user.id;
         },
         displayNewToken: function () {
             this.refresh_token = this.responses.refreshToken;
@@ -142,11 +208,29 @@ var vm = new Vue({
                     this.searchResult[i] = {
                         track: this.responses.search.tracks.items[i].name,
                         artist: this.responses.search.tracks.items[i].artists[0].name,
-                        cover: this.responses.search.tracks.items[i].album.images[0].url
+                        cover: this.responses.search.tracks.items[i].album.images[0].url,
+                        id: this.responses.search.tracks.items[i].id
                     }
                 }
             }
             this.oldSearchInput = this.searchInput;
         },
+        setQueueId: function () {
+            this.queue_id = this.responses.playlist.id;
+        },
+        finished: function () {
+            //if (this.responses.player.progress_ms >= this.responses.player.item.)
+            console.log(this.responses.player);
+        },
+        isPlaying: function () {
+            return this.responses.player.is_playing;
+        },
+        setSongHasStarted: function () {
+            if (this.responses.player.progress_ms > 0) {
+                this.songHasStarted = true;
+            } else {
+                this.songHasStarted = false;
+            }
+        }
     }
 });
